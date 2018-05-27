8fdda34aa4f2e29acdf64e6e4f1dd3ef0e4d5173
Yayan <hilarymanuel@gmail.com>
2018-05-27 04:11:28 -0400

    DOM manipulation practice 1

--- notes-app.js
+++ notes-app.js
@@ -1,1 +1,15 @@
-console.log('Message from the Notes App')
+// DOM - document object model
+
+// // query and remove
+// const p = document.querySelector('p')
+// console.log(p)
+// p.remove()
+
+// query all and remove
+const ps = document.querySelectorAll('p')
+
+ps.forEach(function(p){
+  p.textContent = '=(^-^)='
+  // console.log(p.textContent)
+  // p.remove()
+})
