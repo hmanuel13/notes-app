be5beb84c9ad0632e7ce1232ab04c98a67405f08
Yayan <hilarymanuel@gmail.com>
2018-05-28 08:44:02 -0400

    tentative

--- notes-app.js
+++ notes-app.js
@@ -1,15 +1,21 @@
-// DOM - document object model
+const notes = []
 
-// // query and remove
-// const p = document.querySelector('p')
-// console.log(p)
-// p.remove()
+note1 = {title: 'discover minimalism', body: 'read more about minimalism'}
+note2 = {title: 'design tiles', body: 'why, it\'s just fascinating'}
+note3 = {title: 'adobe', body: 'which tools to study for graphic design'}
+note4 = {title: 'SASS', body: 'why learn SASS?'}
+notes.push(note1, note2, note3, note4)
 
-// query all and remove
 const ps = document.querySelectorAll('p')
 
 ps.forEach(function(p){
-  p.textContent = '=(^-^)='
-  // console.log(p.textContent)
-  // p.remove()
+  p.textContent = '******'
 })
+
+// add a new element
+
+const newP = document.createElement('p')
+
+newP.textContent = 'This is a new Element from Javascript'
+
+document.querySelector('body').appendChild(newP)
